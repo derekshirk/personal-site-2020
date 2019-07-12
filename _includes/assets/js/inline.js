ba8f07d8cf79f825d2ab7cab55db6cd0da0ffa4b
Derek <derekshirk@gmail.com>
2019-07-11 22:26:22 -0700

    adds social media data content and sample theme switcher

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,29 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  const themeSwitchers = document.querySelectorAll('.js-theme-switcher');
+
+  const handleThemeUpdate = (cssVars) => {
+    const root = document.querySelector(':root');
+    const keys = Object.keys(cssVars);
+    keys.forEach(key => {
+      root.style.setProperty(key, cssVars[key]);
+    });
+  }
+
+  themeSwitchers.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const bgColor = e.target.getAttribute('data-bg-color')
+      const color = e.target.getAttribute('data-color')
+      handleThemeUpdate({
+        '--primary-theme-background-color': bgColor,
+        '--primary-theme-text-color': color
+      });
+      console.log(bgColor, color);
+    });
+  });
+
+});
