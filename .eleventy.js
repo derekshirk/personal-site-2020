f05c6d39c9cb1041df744f720cb427aefcf829f6
Derek <derekshirk@gmail.com>
2019-11-11 23:17:25 -0800

    adds variable fonts

--- .eleventy.js
+++ .eleventy.js
@@ -79,6 +79,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static/fonts");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
