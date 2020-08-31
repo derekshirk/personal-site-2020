839b964152b77cd1afedb07a2ea6ec0f04803131
Derek Shirk <derekshirk@dereks-imac.lan>
2020-08-31 03:13:30 -0700

    adds cursor tracking to whale illustration pupil

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -49,6 +49,29 @@ if (localStorage.getItem("userColorMaxContrast") !== null) {
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  const whale = document.querySelector('.Illustration-whale');
+  if (whale) {
+    console.log('thar she blows');
+    let eyeBall = document.querySelector('.eyeball'),
+    pupil = document.querySelector('.pupil'),
+    eyeArea = eyeBall.getBoundingClientRect(),
+    pupilArea = pupil.getBoundingClientRect(),
+    R = eyeArea.width/2,
+    r = pupilArea.width/2,
+    centerX = eyeArea.left + R,
+    centerY = eyeArea.top + R;
+
+    document.addEventListener("mousemove", (e)=>{
+    let x = e.clientX - centerX,
+        y = e.clientY - centerY,
+        theta = Math.atan2(y,x),
+        angle = theta*180/Math.PI + 360;
+
+    pupil.style.transform = `translateX(${R - r +"px"}) rotate(${angle + "deg"})`;
+    pupil.style.transformOrigin = `${r +"px"} center`;
+    }); 
+  }
+
   const themeSwitchers = document.querySelectorAll('.js-theme-switcher');
   const lightModeBtn = document.getElementById('light-mode');
   const darkModeBtn = document.getElementById('dark-mode');
